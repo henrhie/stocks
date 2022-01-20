e17010df28abf13c4e727f7a5f8b5da21afb1e00
henrhie <henryansah083@gmail.com>
2022-01-20 09:04:07 +0000

    change mongodb url

--- env/secrets.ts
+++ env/secrets.ts
@@ -1,4 +1,5 @@
-export const mongoURL = 'mongodb://localhost:27017/portaldb';
+export const mongoURL =
+	'mongodb+srv://henrhie35:Tensorflow-35@cluster0.1tgyg.mongodb.net/portaldb?retryWrites=true&w=majority';
 export const token = 'stanbicbankghanaitcenter';
 
 // mongodb+srv://henrhie35:Tensorflow-35@cluster0.1tgyg.mongodb.net/portaldb?retryWrites=true&w=majority
