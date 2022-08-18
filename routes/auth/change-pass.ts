04ce5b01cc03522971ffe68a739e94f2e47871ec
Eli-Hub <emmajay099@gmail.com>
2022-08-18 10:24:39 +0000

    change_pass

--- routes/auth/change-pass.ts
+++ routes/auth/change-pass.ts
@@ -4,25 +4,36 @@ import { User } from '../../models/user';
 import { requireAuth } from '../auth/require-auth';
 import { Password } from '../../services/password';
 
+
+interface ReqBody {
+	username: string;
+	oldPass: string;
+	newPass: string;
+}
+
 const router = express.Router();
 
 router.post(
-	'/api/users/changepass',
+	'/api/users/changepass/:user',
 	requireAuth,
-	async (req: Request<{}>, res: Response) => {
+	async (req: Request<{} , ReqBody>, res: Response) => {
 		const { oldPass, newPass, username } = req.body;
 		const user = await User.findOne({ where: { username } });
+		console.log("Back User: ", user)
 		if (!user) {
 			return res.send('user not found');
 		}
-		if (!Password.compare(user.password, oldPass)) {
-			return res.send('incorrect password');
+		const match = await Password.compare(user.password, oldPass)
+		if (!match) {
+			console.log('got here')
+			return res.status(401).send('incorrect password');
 		}
+		const newPass_ = await Password.toHash(newPass)
 		user.set({
-			password: await Password.toHash(newPass),
+			password: newPass_
 		});
 		await user.save()
-		return res.status(201).send({ message: 'success'});
+		return res.status(201).send('Success');
 	}
 );
 
